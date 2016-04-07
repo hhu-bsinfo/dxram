e470020eb849a5f1e836abfb9e137f53c6dd18e9
Mike Birkhoff <michael.birkhoff@hhu.de>
2016-04-07 13:19:43 +0200

    changing log level now implemented

--- src/de/hhu/bsinfo/dxram/logger/tcmds/TcmdChangeLogLevel.java
+++ src/de/hhu/bsinfo/dxram/logger/tcmds/TcmdChangeLogLevel.java
@@ -3,10 +3,13 @@ package de.hhu.bsinfo.dxram.logger.tcmds;
 import de.hhu.bsinfo.dxram.chunk.ChunkService;
 import de.hhu.bsinfo.dxram.term.TerminalCommand;
 import de.hhu.bsinfo.utils.args.ArgumentList;
-import de.hhu.bsinfo.dxram.logger.LoggerService;;
+import de.hhu.bsinfo.utils.args.ArgumentList.Argument;
+import de.hhu.bsinfo.dxram.logger.LoggerService;
 
 public class TcmdChangeLogLevel extends TerminalCommand{
 
+	private static final Argument MS_ARG_LEVEL = new Argument("level", null, false, "available LogLevels: DISABLED ERROR WARN INFO DEBUG TRACE");
+	
 	@Override
 	public String getName() {
 		
@@ -21,17 +24,19 @@ public class TcmdChangeLogLevel extends TerminalCommand{
 
 	@Override
 	public void registerArguments(ArgumentList p_arguments) {
-		// TODO Auto-generated method stub	
+		p_arguments.setArgument(MS_ARG_LEVEL);	
 	}
 
 	@Override
 	public boolean execute(ArgumentList p_arguments) {
 		
+		String level =  p_arguments.getArgumentValue(MS_ARG_LEVEL, String.class);
+		
+		if(level == null)
+			return false;
+		
 		LoggerService logService = getTerminalDelegate().getDXRAMService(LoggerService.class);
-		
-		
-		
-//		logService.
+		logService.setLogLevel(level);
 		
 		return true;
 	}
