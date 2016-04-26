b41ecbbdb806c21d577ef96b80a9abc911e4a28d
Beineke, Kevin (kebei100) <kevin.beineke@uni-duesseldorf.de>
2016-04-26 13:28:08 +0200

    Added scripts to disable and enable the logger. With disabled logger the performance increases dramatically (more than 2x in ycsb), but no messages are logged for debugging/tracing.

---
+++ enable_logger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#####################################
+# This script re-enables the logger #
+#####################################
+
+
+find src/ -name "*.java" -print | while read file
+do
+  sed -i "s/\/\/delbysc//g" $file
+done
