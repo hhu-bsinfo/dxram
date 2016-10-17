d1898dda30317fbc0353db5ae5623aa23cefd7d3
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2016-10-17 13:17:40 +0200

    Terminal added shutdown, Removed reboot option from boot

---
+++ script/term/nodeshutdown.js
@@ -0,0 +1,25 @@
+function help() {
+	return "Shutdown a DXRAM node\n" +
+			"Parameters: nid kill\n" +
+			"  nid: Id of the node to shutdown\n" +
+			"  kill: If specified, true for a hard shutdown (kill process), false for proper soft shutdown (default)"
+}
+
+function exec(nid, kill) {
+	var boot = dxram.service("boot")
+
+	if (!nid) {
+		print("No nodeID specified")
+		return
+	} 
+
+	if (!kill) {
+		kill = false
+	}
+
+	if (!boot.shutdownNode(nid, kill)) {
+		print("Shutting down node " + dxram.nidhexstr(nid) + " failed")
+	} else {
+		print("Shutting down node " + dxram.nidhexstr(nid) + "...")
+	}
+}
