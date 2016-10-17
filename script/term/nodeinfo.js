34def7a8b067219feea0059ee9eac9e987538aab
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2016-10-17 12:42:08 +0200

    Terminal scripts: nodeinfo, nodelist, nodereboot

--- script/term/nodeinfo.js
+++ script/term/nodeinfo.js
@@ -1,19 +1,22 @@
 function help() {
-	return "Get information about either the current node or another node in the network"
+	return "Get information about either the current node or another node in the network\n" +
+			"Parameters: nid\n" +
+			"  nid: If specified, gets information of this node"
 }
 
-function exec(nodeID) {
+function exec(nid) {
 	var boot = dxram.service("boot")
 
-	print("Node info " + dxram.nodeIdToHexString(nodeID) + ":")
-	if (nodeID) {
-		if (boot.nodeAvailable(nodeID)) {
-			print("\tRole: " + boot.getNodeRole(nodeID));
-			print("\tAddress: " + boot.getNodeAddress(nodeID));
+	if (nid) {
+		if (boot.nodeAvailable(nid)) {
+			print("Node info " + dxram.nidhexstr(nid) + ":")
+			print("\tRole: " + boot.getNodeRole(nid));
+			print("\tAddress: " + boot.getNodeAddress(nid));
 		} else {
 			print("Not available.");
 		}
 	} else {
+		print("Node info " + dxram.nidhexstr(boot.getNodeID()) + ":")
 		print("\tRole: " + boot.getNodeRole())
 		print("\tAddress: " + boot.getNodeAddress(boot.getNodeID()))
 	}
---
+++ script/term/nodelist.js
@@ -0,0 +1,27 @@
+function help() {
+	return "List all available nodes or nodes of a specific type\n" +
+			"Parameters: role\n" +
+			"  role: Filter list by role if specified"
+}
+
+function exec(role) {
+
+	var boot = dxram.service("boot")
+	var nodeRole = dxram.noderole(role)
+
+	var nodeIds = boot.getOnlineNodeIDs()
+
+	if (role) {			
+		print("Filtering by role " + nodeRole)
+	} 
+
+	print("Total available nodes (" + nodeIds.size() + "):")
+		
+	for each(nodeId in nodeIds) {
+		var curRole = boot.getNodeRole(nodeId)
+		
+		if (!role || role && nodeRole.equals(curRole)) {
+			print("\t" + dxram.nidhexstr(nodeId) + ", " + curRole + ", " + boot.getNodeAddress(nodeId))
+		}
+	}
+}
