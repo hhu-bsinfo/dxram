23c405871032d0c85b43350d0cbd14e316a1d736
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2016-10-19 09:30:08 +0200

    Terminal cmd: tmpput

---
+++ script/term/tmpput.js
@@ -0,0 +1,137 @@
+function help() {
+
+	return "Put data in the specified chunk (temporary storage)." +
+            "If no offset is specified, the whole chunk is overwritten with the new data. " +
+            "Otherwise the data is inserted at the starting offset with its length. " +
+            "If the specified data is too long it will be trunced\n" +
+			"Parameters: id data [offset] [type]\n" +
+			"  id: Id of the chunk stored in temporary storage\n" +
+			"  data: Data to store (format has to match type parameter)\n" +
+			"  type: Type of the data to store (str, byte, short, int, long, hex), defaults to str\n" +
+			"  offset: Offset within the existing to store the new data to. -1 to override existing data, defaults to 0";
+}
+
+function exec(id, data, offset, type) {
+
+    if (id == null) {
+        dxterm.printlnErr("No id specified");
+        return;
+    }
+
+    if (data == null) {
+        dxterm.printlnErr("No data specified");
+        return;
+    }
+
+    if (offset == null) {
+        offset = 0;
+    }
+
+    if (type == null) {
+        type = "str";
+    }
+    type = type.toLowerCase();
+
+    var tmpstore = dxram.service("tmpstore");
+    var chunk = tmpstore.get(id);
+
+    if (chunk == null) {
+        dxmterm.printlnErr("Getting chunk " + dxram.intToHexStr(id) + " from tmp storage failed.");
+        return;
+    }
+
+    if (offset == -1) {
+        // create new chunk
+        chunk = dxram.newChunk(chunk.getID(), chunk.getDataSize());
+        offset = 0;
+    }
+
+    if (offset > chunk.sizeofObject()) {
+        offset = chunk.sizeofObject();
+    }
+
+    var buffer = chunk.getData();
+    buffer.position(offset);
+
+    switch (type) {
+
+        case "str":
+            var bytes = data.getBytes(java.nio.charset.StandardCharsets.US_ASCII);
+
+            try {
+                var size = buffer.capacity() - buffer.position();
+                if (bytes.length < size) {
+                    size = bytes.length;
+                }
+                buffer.put(bytes, 0, size);
+            } catch (e) {
+                // that's fine, trunc data
+            }
+            break;
+
+        case "byte":
+            var b = java.lang.Integer.parseInt(data) & 0xFF;
+
+            try {
+                buffer.put(b);
+            } catch (e) {
+                // that's fine, trunc data
+            }
+            break;
+
+        case "short":
+            var v = java.lang.Integer.parseInt(data) & 0xFFFF;
+
+            try {
+                buffer.putShort(v);
+            } catch (e) {
+                // that's fine, trunc data
+            }
+            break;
+
+        case "int":
+            var v = java.lang.Integer.parseInt(data) & 0xFFFFFFFF;
+
+            try {
+                buffer.putInt(v);
+            } catch (e) {
+                // that's fine, trunc data
+            }
+            break;
+
+        case "long":
+            var v = java.lang.Long.parseLong(data) & 0xFFFFFFFFFFFFFFFF;
+
+            try {
+                buffer.putLong(v);
+            } catch (e) {
+                // that's fine, trunc data
+            }
+            break;
+
+         case "hex":
+            var tokens = data.split(" ");
+
+            for each (var token in tokens) {
+                var v = java.lang.Integer.parseInt(token, 16);
+                try {
+                    buffer.put(v);
+                } catch (e) {
+                    // that's fine, trunc data
+                    break;
+                }
+            }
+            break;
+
+        default:
+            dxterm.printlnErr("Unsupported data type " + type);
+            return;
+    }
+
+    // put chunk back
+    if (tmpstore.put(chunk) != 1) {
+        dxterm.printlnErr("Putting chunk " + dxram.intToHexStr(id) + " to tmp storage failed");
+    } else {
+        dxterm.println("Put to chunk " + dxram.intToHexStr(id) + " to tmp storage successful.");
+    }
+}
