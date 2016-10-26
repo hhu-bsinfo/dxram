d15ea100fdc4843ac875a81cb594f7c5c6bba7d0
Beineke, Kevin (kebei100) <kevin.beineke@uni-duesseldorf.de>
2016-10-26 17:56:48 +0200

    - NodesConfiguration: marked m_readFromFile as configuration value and added default constructor

--- src/de/hhu/bsinfo/dxram/boot/NodesConfiguration.java
+++ src/de/hhu/bsinfo/dxram/boot/NodesConfiguration.java
@@ -2,6 +2,7 @@
 package de.hhu.bsinfo.dxram.boot;
 
 import com.google.gson.annotations.Expose;
+
 import de.hhu.bsinfo.dxram.util.NodeRole;
 import de.hhu.bsinfo.ethnet.NodeID;
 import de.hhu.bsinfo.utils.unit.IPV4Unit;
@@ -9,13 +10,12 @@ import de.hhu.bsinfo.utils.unit.IPV4Unit;
 /**
  * Represents a nodes configuration for DXRAM. This also holds any information
  * about the current node as well as any remote nodes available in the system.
- *
  * @author Florian Klein
  *         03.09.2013
  * @author Stefan Nothaas <stefan.nothaas@hhu.de> 9.12.15
  */
 public final class NodesConfiguration {
-	
+
 	private NodeEntry[] m_nodes = new NodeEntry[NodeID.MAX_ID + 1];
 	private short m_ownID = NodeID.INVALID_ID;
 
@@ -28,7 +28,6 @@ public final class NodesConfiguration {
 
 	/**
 	 * Gets the configured node
-	 *
 	 * @return the configured nodes
 	 */
 	public NodeEntry[] getNodes() {
@@ -37,8 +36,8 @@ public final class NodesConfiguration {
 
 	/**
 	 * Get the NodeEntry of the specified node ID.
-	 *
-	 * @param p_nodeID Node ID to get the entry of.
+	 * @param p_nodeID
+	 *            Node ID to get the entry of.
 	 * @return NodeEntry containing information about the node or null if it does not exist.
 	 */
 	public NodeEntry getNode(final short p_nodeID) {
@@ -47,7 +46,6 @@ public final class NodesConfiguration {
 
 	/**
 	 * Get the node ID which is set for this node.
-	 *
 	 * @return Own node ID (or -1 if invalid).
 	 */
 	public short getOwnNodeID() {
@@ -56,7 +54,6 @@ public final class NodesConfiguration {
 
 	/**
 	 * Get the NodeEntry corresponding to our node ID.
-	 *
 	 * @return NodeEntry or null if invalid.
 	 */
 	public NodeEntry getOwnNodeEntry() {
@@ -67,9 +64,10 @@ public final class NodesConfiguration {
 
 	/**
 	 * Adds a node
-	 *
-	 * @param p_nodeID Id of the node.
-	 * @param p_entry  the configured node
+	 * @param p_nodeID
+	 *            Id of the node.
+	 * @param p_entry
+	 *            the configured node
 	 */
 	synchronized void addNode(final short p_nodeID, final NodeEntry p_entry) {
 		m_nodes[p_nodeID & 0xFFFF] = p_entry;
@@ -77,8 +75,8 @@ public final class NodesConfiguration {
 
 	/**
 	 * Remove a node from the mappings list.
-	 *
-	 * @param p_nodeID Node ID of the entry to remove.
+	 * @param p_nodeID
+	 *            Node ID of the entry to remove.
 	 */
 	synchronized void removeNode(final short p_nodeID) {
 		m_nodes[p_nodeID & 0xFFFF] = null;
@@ -86,8 +84,8 @@ public final class NodesConfiguration {
 
 	/**
 	 * Set the node ID for the current/own node.
-	 *
-	 * @param p_nodeID Node id to set.
+	 * @param p_nodeID
+	 *            Node id to set.
 	 */
 	synchronized void setOwnNodeID(final short p_nodeID) {
 		m_ownID = p_nodeID;
@@ -109,7 +107,6 @@ public final class NodesConfiguration {
 
 	/**
 	 * Describes a nodes configuration entry
-	 *
 	 * @author Florian Klein
 	 *         03.09.2013
 	 */
@@ -124,18 +121,26 @@ public final class NodesConfiguration {
 		private short m_rack = 0;
 		@Expose
 		private short m_switch = 0;
-
-		// private state
-		private byte m_readFromFile;
+		@Expose
+		private byte m_readFromFile = 1;
 
 		/**
 		 * Creates an instance of NodesConfigurationEntry
-		 *
-		 * @param p_address		 addres of the node
-		 * @param p_rack         the rack of the node
-		 * @param p_switch       the switcharea of the node
-		 * @param p_role         the role of the node
-		 * @param p_readFromFile whether this node's information was read from nodes file or not
+		 */
+		NodeEntry() {}
+
+		/**
+		 * Creates an instance of NodesConfigurationEntry
+		 * @param p_address
+		 *            addres of the node
+		 * @param p_rack
+		 *            the rack of the node
+		 * @param p_switch
+		 *            the switcharea of the node
+		 * @param p_role
+		 *            the role of the node
+		 * @param p_readFromFile
+		 *            whether this node's information was read from nodes file or not
 		 */
 		NodeEntry(final IPV4Unit p_address, final short p_rack, final short p_switch,
 				final NodeRole p_role, final boolean p_readFromFile) {
@@ -152,7 +157,6 @@ public final class NodesConfiguration {
 
 		/**
 		 * Gets the address of the node
-		 *
 		 * @return the address of the node
 		 */
 		public IPV4Unit getAddress() {
@@ -161,7 +165,6 @@ public final class NodesConfiguration {
 
 		/**
 		 * Gets the rack of the node
-		 *
 		 * @return the rack of the node
 		 */
 		public short getRack() {
@@ -170,7 +173,6 @@ public final class NodesConfiguration {
 
 		/**
 		 * Gets the switcharea of the node
-		 *
 		 * @return the switcharea of the node
 		 */
 		public short getSwitch() {
@@ -179,7 +181,6 @@ public final class NodesConfiguration {
 
 		/**
 		 * Gets the role of the node
-		 *
 		 * @return the role of the node
 		 */
 		public NodeRole getRole() {
@@ -188,7 +189,6 @@ public final class NodesConfiguration {
 
 		/**
 		 * Gets the source of the node's information
-		 *
 		 * @return whether this node's information was read from nodes file or not
 		 */
 		public boolean readFromFile() {
