c02c842986fc0eee73adac24394215d719fc9216
Beineke, Kevin (kebei100) <kevin.beineke@uni-duesseldorf.de>
2016-11-29 16:21:52 +0100

    - added short license to all javascripts and c-jni files

--- jni/harddriveaccess/JNIFileDirect.c
+++ jni/harddriveaccess/JNIFileDirect.c
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2016 Heinrich-Heine-Universitaet Duesseldorf, Institute of Computer Science, Department Operating Systems
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 // Compile: gcc -O2 -m64 -o ../libJNIFileDirect.so -fpic -shared -I/usr/lib/jvm/java-8-oracle/include/ -I/usr/lib/jvm/java-8-oracle/include/linux ./JNIFileDirect.c
 
 /* This is the implementation of access to logfiles via JNI and O_DIRECT, so that
--- jni/harddriveaccess/JNIFileRaw.c
+++ jni/harddriveaccess/JNIFileRaw.c
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2016 Heinrich-Heine-Universitaet Duesseldorf, Institute of Computer Science, Department Operating Systems
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 // Compile: gcc -O2 -m64 -o ../libJNIFileRaw.so -fpic -shared -I/usr/lib/jvm/java-8-oracle/include/ -I/usr/lib/jvm/java-8-oracle/include/linux ./JNIFileRaw.c
 
 /* This is an implementation of raw-device access bypassing the page cache of linux.
