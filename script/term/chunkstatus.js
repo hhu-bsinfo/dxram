6169fa7f418f9e0bf6967b24b2e76b6725d6ef0a
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-01-31 10:44:45 +0100

    Term script: Fix chunkstatus script

--- script/term/chunkstatus.js
+++ script/term/chunkstatus.js
@@ -17,20 +17,15 @@ function imports() {
 
 function help() {
 	return "Get the status of the chunk service/memory from a remote node\n" +
-			"Usage: chunkstatus(nid, sizetype)\n" +
-			"  nid: Node ID of the remote peer to get the status from\n" +
-			"  sizetype: Specify the type of size you want to display (b, kb, mb, gb)";
+			"Usage: chunkstatus(nid)\n" +
+			"  nid: Node ID of the remote peer to get the status from";
 }
 
-function exec(nid, sizetype) {
+function exec(nid) {
 
     if (nid == null) {
         dxterm.printlnErr("No nid specified");
         return;
-    }
-
-    if (sizetype == null) {
-        sizetype = "b";
     }
 
     var chunk = dxram.service("chunk");
@@ -42,44 +37,5 @@ function exec(nid, sizetype) {
         return;
     }
 
-    var divisor = 1;
-    if (sizetype != null) {
-        sizetype = sizetype.toLowerCase();
-        if (sizetype.equals("b")) {
-            divisor = 1;
-        } else if (sizetype.equals("kb")) {
-            divisor = 1024;
-        } else if (sizetype.equals("mb")) {
-            divisor = 1024 * 1024;
-        } else if (sizetype.equals("gb")) {
-            divisor = 1024 * 1024 * 1024;
-        } else {
-            // invalid, default to byte
-            sizetype = "b";
-        }
-    } else {
-        sizetype = "b";
-    }
-
-    dxterm.printfln("Chunk service/memory status of 0x%X:", nid);
-    if (divisor == 1) {
-        dxterm.printfln("Free memory (%s): %d", sizetype, status.getFreeMemory());
-        dxterm.printfln("Total memory (%s): %d", sizetype, status.getTotalMemory());
-        dxterm.printfln("Total payload memory (%s): %d", sizetype, status.getTotalPayloadMemory());
-        dxterm.printfln("Total chunk payload memory (%s): %d", sizetype, status.getTotalChunkPayloadMemory());
-        dxterm.printfln("Total CID tables memory (NID table with 327687) (%s): %d", sizetype,
-                        status.getTotalMemoryCIDTables());
-    } else {
-        dxterm.printfln("Free memory (%s): %d", sizetype, status.getFreeMemory() / divisor);
-        dxterm.printfln("Total memory (%s): %d", sizetype, status.getTotalMemory() / divisor);
-        dxterm.printfln("Total payload memory (%s): %d", sizetype, status.getTotalPayloadMemory() / divisor);
-        dxterm.printfln("Total chunk payload memory (%s): %d", sizetype,
-                        status.getTotalChunkPayloadMemory() / divisor);
-        dxterm.printfln("Total CID tables memory (NID table with 327687) (%s): %d",
-                        sizetype, status.getTotalMemoryCIDTables() / divisor);
-    }
-
-    dxterm.printfln("Num active memory blocks: %d", status.getNumberOfActiveMemoryBlocks());
-    dxterm.printfln("Num active chunks: %d", status.getNumberOfActiveChunks());
-    dxterm.printfln("Num CID tables (one is NID table): %d", status.getCIDTableCount());
+    dxterm.println(status);
 }
