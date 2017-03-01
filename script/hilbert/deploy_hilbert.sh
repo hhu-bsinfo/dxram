2d11ee0206fa6c35809af2d37ea7070e23afa084
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-03-01 13:23:04 +0100

    Hilbert scripts enhancements

--- script/hilbert/deploy_hilbert.sh
+++ script/hilbert/deploy_hilbert.sh
@@ -7,10 +7,13 @@ HILBERT_CPUS_PER_NODE=""
 HILBERT_MEM_PER_NODE_GB=""
 HILBERT_WALL_TIME=""
 HILBERT_JOB_NAME=""
+HILBERT_JOB_SUB_PARAMS=""
 
 TMP=/scratch_gs/$USER/dxram_tmp
-TEMPLATE=dxram_template.job
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEMPLATE="$THIS_SCRIPT_DIR/dxram_template.job"
 
+JOB_WORKING_DIR=""
 JOB_FILE_PATH=""
 DEPLOY_CONFIG_PATH=""
 
@@ -83,28 +86,40 @@ determine_hilbert_parameters()
 	fi
 	HILBERT_JOB_NAME=`echo "$tmp" | cut -d '=' -f 2`
 
+  	local tmp=`echo "$DEPLOY_CONFIG" | grep HILBERT_JOB_SUB_PARAMS`
+  	if [ "$tmp" = "" ] ; then
+    	echo "Missing HILBERT_JOB_SUB_PARAMS in deploy config"
+		exit -1
+	fi
+	HILBERT_JOB_SUB_PARAMS=`echo "$tmp" | cut -d '=' -f 2`
+
   	# Trim config file, remove hilbert parameters
   	DEPLOY_CONFIG=`echo "$DEPLOY_CONFIG" | grep -v 'HILBERT_NODE_COUNT'`
   	DEPLOY_CONFIG=`echo "$DEPLOY_CONFIG" | grep -v 'HILBERT_CPUS_PER_NODE'`
 	DEPLOY_CONFIG=`echo "$DEPLOY_CONFIG" | grep -v 'HILBERT_MEM_PER_NODE_GB'`
 	DEPLOY_CONFIG=`echo "$DEPLOY_CONFIG" | grep -v 'HILBERT_WALL_TIME'`
 	DEPLOY_CONFIG=`echo "$DEPLOY_CONFIG" | grep -v 'HILBERT_JOB_NAME'`
+	DEPLOY_CONFIG=`echo "$DEPLOY_CONFIG" | grep -v 'HILBERT_JOB_SUB_PARAMS'`
 }
 
 create_hilbert_deployable_job()
 {
+	# Working directory for job
+	JOB_WORKING_DIR=$(date +%s)
+	JOB_WORKING_DIR=${JOB_WORKING_DIR}_${HILBERT_JOB_NAME}_${HILBERT_NODE_COUNT}_${HILBERT_CPUS_PER_NODE}
+	JOB_WORKING_DIR="$TMP/$JOB_WORKING_DIR"
+	mkdir $JOB_WORKING_DIR
+	echo "Output/working dir for job data: $JOB_WORKING_DIR"
+
 	# Job template
-	JOB_FILE_PATH="dxram_${HILBERT_JOB_NAME}_${HILBERT_NODE_COUNT}_${HILBERT_CPUS_PER_NODE}.job"
+	JOB_FILE_PATH="$JOB_WORKING_DIR/dxram.job"
+	cp $TEMPLATE $JOB_FILE_PATH
 	echo "Creating deployable hilbert job: $JOB_FILE_PATH"
 
-	JOB_FILE_PATH="$TMP/$JOB_FILE_PATH"
-	cp $TEMPLATE $JOB_FILE_PATH
-
 	# Config file
-	DEPLOY_CONFIG_PATH="dxram_${HILBERT_JOB_NAME}_${HILBERT_NODE_COUNT}_${HILBERT_CPUS_PER_NODE}.conf"
+	DEPLOY_CONFIG_PATH="deploy.conf"
+	DEPLOY_CONFIG_PATH="$JOB_WORKING_DIR/$DEPLOY_CONFIG_PATH"
 	echo "Deploy config for hilbert job: $DEPLOY_CONFIG_PATH"
-
-	DEPLOY_CONFIG_PATH="$TMP/$DEPLOY_CONFIG_PATH"
 
 	if [ -e $DEPLOY_CONFIG_PATH ]; then
 		rm $DEPLOY_CONFIG_PATH
@@ -121,13 +136,13 @@ create_hilbert_deployable_job()
 	sed -i "s/%%%HILBERT_WALL_TIME%%%/$HILBERT_WALL_TIME/g" $JOB_FILE_PATH
 	sed -i "s/%%%HILBERT_JOB_NAME%%%/$HILBERT_JOB_NAME/g" $JOB_FILE_PATH
 
-	sed -i "s#%%%DXRAM_DEPLOY_CONFIG_PATH%%%#${DEPLOY_CONFIG_PATH}#g" $JOB_FILE_PATH
+	sed -i "s#%%%DXRAM_JOB_WORKING_DIR%%%#${JOB_WORKING_DIR}#g" $JOB_FILE_PATH
 }
 
 submit_hilbert_job()
 {
 	echo "Submitting generated job: $JOB_FILE_PATH"
-	qsub $JOB_FILE_PATH
+	qsub $HILBERT_JOB_SUB_PARAMS -o "$JOB_WORKING_DIR/job.log" -j oe $JOB_FILE_PATH
 }
 
 ###############
