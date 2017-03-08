95f4ff8b3941fb80a301361a7e1b9e1982880c78
Beineke, Kevin (kebei100) <kevin.beineke@uni-duesseldorf.de>
2017-03-08 13:32:44 +0100

    - added documentation for native crc generator

--- jni/nativecrcgen/JNINativeCRCGenerator.c
+++ jni/nativecrcgen/JNINativeCRCGenerator.c
@@ -23,16 +23,18 @@
 #include <x86intrin.h>
 
 
-/*uint32_t sse42_crc32(uint32_t p_crc, const uint8_t* p_data, uint32_t p_offset, uint32_t p_length) {
-  uint32_t i = 0;
-  for (i = 0; i < p_length; i++) {
-    p_crc = _mm_crc32_u8(p_crc, p_data[i + p_offset]);
-  }
-  //0x11EDC6F41
-
-  return p_crc;
-}*/
-
+/**
+ * Calculates the checksum with SSE4.2 instructions (polynomial: 0x1EDC6F41)
+ * @param p_crc
+ *      the previous checksum
+ * @param p_data
+ *      the byte array containing the data
+ * @param p_offset
+ *      the offset within byte array
+ * @param p_length
+ *      the length of the data
+ * @return the crc32 checksum
+ */
 uint32_t sse42_crc32(uint32_t p_crc, const uint8_t* p_data, uint32_t p_offset, uint32_t p_length) {
   uint32_t i = 0;
   while (i + 8 <= p_length) {
@@ -58,23 +60,33 @@ uint32_t sse42_crc32(uint32_t p_crc, const uint8_t* p_data, uint32_t p_offset, u
   return p_crc;
 }
 
+/**
+ * Calculates the checksum with zlib (for processors without SSE4.2)
+ * @param p_crc
+ *      the previous checksum
+ * @param p_data
+ *      the byte array containing the data
+ * @param p_offset
+ *      the offset within byte array
+ * @param p_length
+ *      the length of the data
+ * @return the crc32 checksum
+ */
 uint32_t default_crc32(uint32_t p_crc, const uint8_t* p_data, uint32_t p_offset, uint32_t p_length) {
   return crc32(p_crc, p_data + p_offset, p_length);
 }
 
-
-/* this will be called at load time to decide which function really use */
-/* sse42_crc32 if SSE 4.2 is supported */
-/* default_crc32 if not */
-static void * resolve_crc32(void) {
+/**
+ * Checks if SSE4.2 is supported, returns function pointer accordingly
+ */
+static void* resolve_crc32(void) {
   __builtin_cpu_init();
   if (__builtin_cpu_supports("sse4.2")) return sse42_crc32;
 
   return default_crc32;
 }
 
-/* crc32_sse_long() implementation will be resolved at load time to either */
-/* sse42_crc32_long() or default_crc32() */
+
 uint32_t crc32_sse(uint32_t p_crc, const uint8_t* p_data, uint32_t p_offset, uint32_t p_length) __attribute__ ((ifunc ("resolve_crc32")));
 
 
