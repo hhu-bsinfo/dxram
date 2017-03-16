d5719db5af2e5e2755b4de76e60766c184e80c52
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-03-16 08:43:59 +0100

    Add hilbert deploy script to env script

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 BIN=$(pwd)/script/deploy/bin
+BIN_HILBERT=$(pwd)/script/hilbert/bin
 
-export PATH=${PATH}:$BIN
+export PATH=${PATH}:$BIN:$BIN_HILBERT
 
 echo "DXRAM environment configured"
 bash
