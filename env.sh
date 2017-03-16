e9ab063223496258fdb63a925441a21d6995ab93
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-03-16 08:48:36 +0100

    env script: bugfixes

--- env.sh
+++ env.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-BIN=$(pwd)/script/deploy/bin
-BIN_HILBERT=$(pwd)/script/hilbert/bin
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+BIN=$DIR/script/deploy/bin
+SCRIPTS=$DIR/script/deploy
+SCRIPTS_MODS=$DIR/script/deploy/modules
+BIN_HILBERT=$DIR/script/hilbert/bin
+HILBERT_SCRIPTS=$DIR/script/hilbert
+
+# Make all scripts executable
+chmod +x $BIN/*
+chmod +x $SCRIPTS/*
+chmod +x $SCRIPTS_MODS/*
+chmod +x $BIN_HILBERT/*
+chmod +x $HILBERT_SCRIPTS/*
 
 export PATH=${PATH}:$BIN:$BIN_HILBERT
 
