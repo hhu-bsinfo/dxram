9394dde59259fe11dfd6af69128f3342a66d82f4
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-05-17 15:23:28 +0200

    Minor bugfix

--- src/de/hhu/bsinfo/dxram/job/JobNull.java
+++ src/de/hhu/bsinfo/dxram/job/JobNull.java
@@ -18,6 +18,7 @@ import org.apache.logging.log4j.Logger;
 
 /**
  * Example for a job implementation.
+ *
  * @author Stefan Nothaas, stefan.nothaas@hhu.de, 03.02.2016
  */
 public class JobNull extends AbstractJob {
@@ -25,10 +26,6 @@ public class JobNull extends AbstractJob {
     private static final Logger LOGGER = LogManager.getFormatterLogger(JobNull.class.getSimpleName());
 
     public static final short MS_TYPE_ID = 0;
-
-    static {
-        registerType(MS_TYPE_ID, JobNull.class);
-    }
 
     /**
      * Constructor
