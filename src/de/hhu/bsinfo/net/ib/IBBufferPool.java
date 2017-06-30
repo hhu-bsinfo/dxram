ddee76e14b08d13ed8a89c21d0bcd85084356e85
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-06-30 13:04:57 +0200

    IBBufferPool: Important bugfixes

--- src/de/hhu/bsinfo/net/ib/IBBufferPool.java
+++ src/de/hhu/bsinfo/net/ib/IBBufferPool.java
@@ -1,31 +1,51 @@
 package de.hhu.bsinfo.net.ib;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class IBBufferPool {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-    private ByteBuffer[] m_pool;
+public class IBBufferPool {
+    private static final Logger LOGGER = LogManager.getFormatterLogger(IBBufferPool.class.getSimpleName());
+
+    private ArrayList<ByteBuffer> m_pool;
+    private int m_bufferSize;
     private int m_pos;
+    private int m_poolSize;
     private ReentrantLock m_lock;
 
     public IBBufferPool(final int p_bufferSize, final int p_poolSize) {
-        m_pool = new ByteBuffer[p_poolSize];
-        m_pos = 0;
+        m_pool = new ArrayList<ByteBuffer>();
+        m_bufferSize = p_bufferSize;
+        m_pos = p_poolSize - 1;
+        m_poolSize = p_poolSize;
         m_lock = new ReentrantLock(false);
 
-        for (int i = 0; i < m_pool.length; i++) {
-            m_pool[i] = ByteBuffer.allocateDirect(p_bufferSize);
+        for (int i = 0; i < m_poolSize; i++) {
+            m_pool.add(ByteBuffer.allocateDirect(p_bufferSize));
         }
     }
 
     public ByteBuffer getBuffer() {
-        ByteBuffer buffer = null;
+        ByteBuffer buffer;
 
         m_lock.lock();
 
-        if (m_pos < m_pool.length) {
-            buffer = m_pool[m_pos++];
+        if (m_pos >= 0) {
+            buffer = m_pool.get(m_pos);
+            m_pos--;
+        } else {
+            // #if LOGGER >= WARN
+            LOGGER.warn("Insufficient pooled incoming buffers. Expanding pool...");
+            // #endif /* LOGGER >= WARN */
+
+            buffer = ByteBuffer.allocateDirect(m_bufferSize);
+
+            // ensures that the ArrayList is expanded
+            m_pool.add(null);
+            m_poolSize++;
         }
 
         m_lock.unlock();
@@ -36,11 +56,14 @@ public class IBBufferPool {
     public void returnBuffer(final ByteBuffer p_buffer) {
         m_lock.lock();
 
-        if (m_pos == 0) {
-            throw new IllegalStateException("Position underflow");
+        if (m_pos + 1 >= m_poolSize) {
+            m_lock.unlock();
+            throw new IllegalStateException("Position overflow");
         }
 
-        m_pool[--m_pos] = p_buffer;
+        m_pos++;
+
+        m_pool.set(m_pos, p_buffer);
 
         m_lock.unlock();
     }
