de519df5ff0fbf5639d6b64970e498d8eacb166e
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2017-08-02 13:21:55 +0200

    utils: Add copyright header (where missing)

--- src/de/hhu/bsinfo/utils/RandomUtils.java
+++ src/de/hhu/bsinfo/utils/RandomUtils.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2017 Heinrich-Heine-Universitaet Duesseldorf, Institute of Computer Science, Department Operating Systems
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package de.hhu.bsinfo.utils;
 
 import de.hhu.bsinfo.utils.unit.StorageUnit;
@@ -21,9 +34,9 @@ public final class RandomUtils {
      * Get a random size from a size range
      *
      * @param p_start
-     *     Start of size range (including)
+     *         Start of size range (including)
      * @param p_end
-     *     End of size range (including)
+     *         End of size range (including)
      * @return Random size in bytes
      */
     public static int getRandomSize(final StorageUnit p_start, final StorageUnit p_end) {
@@ -34,7 +47,7 @@ public final class RandomUtils {
      * Get a random number in the range of 0 to a specific end
      *
      * @param p_end
-     *     End (including)
+     *         End (including)
      * @return Random int
      */
     public static int getRandomValue(final int p_end) {
@@ -45,9 +58,9 @@ public final class RandomUtils {
      * Get a random number from a specified range
      *
      * @param p_start
-     *     Start (including)
+     *         Start (including)
      * @param p_end
-     *     End (including)
+     *         End (including)
      * @return Random int
      */
     public static int getRandomValue(final int p_start, final int p_end) {
@@ -58,9 +71,9 @@ public final class RandomUtils {
      * Get a random number from a specified range
      *
      * @param p_start
-     *     Start (including)
+     *         Start (including)
      * @param p_end
-     *     End (excluding)
+     *         End (excluding)
      * @return Random int
      */
     public static int getRandomValueExclEnd(final int p_start, final int p_end) {
@@ -71,9 +84,9 @@ public final class RandomUtils {
      * Get a random number from a specified range
      *
      * @param p_start
-     *     Start (including)
+     *         Start (including)
      * @param p_end
-     *     End (including)
+     *         End (including)
      * @return Random int
      */
     public static long getRandomValue(final long p_start, final long p_end) {
--- src/de/hhu/bsinfo/utils/StringUtils.java
+++ src/de/hhu/bsinfo/utils/StringUtils.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2017 Heinrich-Heine-Universitaet Duesseldorf, Institute of Computer Science, Department Operating Systems
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package de.hhu.bsinfo.utils;
 
 import java.io.UnsupportedEncodingException;
@@ -22,21 +35,22 @@ public final class StringUtils {
      * Checks if a Byte array contains a String (as much precise as possible)
      *
      * @param p_array
-     *     the byte array
+     *         the byte array
      * @return whether the byte array contains a String
      * @throws UnsupportedEncodingException
-     *     if the encoding is unsupported
+     *         if the encoding is unsupported
      */
     public static boolean looksLikeUTF8(final byte[] p_array) throws UnsupportedEncodingException {
         String phonyString;
         Pattern p;
 
         p = Pattern.compile(
-            "\\A(\n" + "  [\\x09\\x0A\\x0D\\x20-\\x7E]             # ASCII\\n" + "| [\\xC2-\\xDF][\\x80-\\xBF]               # non-overlong 2-byte\n" +
-                "|  \\xE0[\\xA0-\\xBF][\\x80-\\xBF]         # excluding overlongs\n" + "| [\\xE1-\\xEC\\xEE\\xEF][\\x80-\\xBF]{2}  # straight 3-byte\n" +
-                "|  \\xED[\\x80-\\x9F][\\x80-\\xBF]         # excluding surrogates\n" + "|  \\xF0[\\x90-\\xBF][\\x80-\\xBF]{2}      # planes 1-3\n" +
-                "| [\\xF1-\\xF3][\\x80-\\xBF]{3}            # planes 4-15\n" + "|  \\xF4[\\x80-\\x8F][\\x80-\\xBF]{2}      # plane 16\n" + ")*\\z",
-            Pattern.COMMENTS);
+                "\\A(\n" + "  [\\x09\\x0A\\x0D\\x20-\\x7E]             # ASCII\\n" + "| [\\xC2-\\xDF][\\x80-\\xBF]               # non-overlong 2-byte\n" +
+                        "|  \\xE0[\\xA0-\\xBF][\\x80-\\xBF]         # excluding overlongs\n" +
+                        "| [\\xE1-\\xEC\\xEE\\xEF][\\x80-\\xBF]{2}  # straight 3-byte\n" +
+                        "|  \\xED[\\x80-\\x9F][\\x80-\\xBF]         # excluding surrogates\n" + "|  \\xF0[\\x90-\\xBF][\\x80-\\xBF]{2}      # planes 1-3\n" +
+                        "| [\\xF1-\\xF3][\\x80-\\xBF]{3}            # planes 4-15\n" + "|  \\xF4[\\x80-\\x8F][\\x80-\\xBF]{2}      # plane 16\n" + ")*\\z",
+                Pattern.COMMENTS);
 
         phonyString = new String(p_array, "UTF-8");
 
