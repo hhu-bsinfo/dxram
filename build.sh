38052e27d2a59fbf3f5477196dc84e0169c8d359
Stefan Nothaas <stefan.nothaas@uni-duesseldorf.de>
2018-10-20 20:57:35 +0200

    build.sh: Call clean to avoid build inconsistencies

--- build.sh
+++ build.sh
@@ -12,8 +12,10 @@ case "$build_type" in
     debug)
         ;;
     release)
+	TERM=dump ./gradlew clean
         ;;
     performance)
+	TERM=dump ./gradlew clean
         ;;
     clean)
         clean="1"        
