7e7408c23c84cbaf6419232f248d7300bf908708
saversux <julien.bernhart@uni-duesseldorf.de>
2019-04-26 13:35:43 +0200

    better serialization in sync messages

--- src/main/java/de/hhu/bsinfo/dxram/loader/messages/SyncRequestMessage.java
+++ src/main/java/de/hhu/bsinfo/dxram/loader/messages/SyncRequestMessage.java
@@ -6,13 +6,12 @@ import java.util.Set;
 import de.hhu.bsinfo.dxnet.core.AbstractMessageExporter;
 import de.hhu.bsinfo.dxnet.core.AbstractMessageImporter;
 import de.hhu.bsinfo.dxnet.core.Message;
-import de.hhu.bsinfo.dxnet.core.Request;
 import de.hhu.bsinfo.dxram.DXRAMMessageTypes;
-import de.hhu.bsinfo.dxram.loader.ObjectSerializer;
 import de.hhu.bsinfo.dxutils.serialization.ObjectSizeUtil;
 
 public class SyncRequestMessage extends Message {
-    private byte[] m_loadedJars;
+    private Set<String> m_loadedJars;
+    private int m_setSize;
 
     public SyncRequestMessage() {
         super();
@@ -20,25 +19,44 @@ public class SyncRequestMessage extends Message {
 
     public SyncRequestMessage(final short p_destination, Set<String> p_loadedJars) {
         super(p_destination, DXRAMMessageTypes.LOADER_MESSAGE_TYPE, LoaderMessages.SUBTYPE_SYNC_REQUEST);
-        m_loadedJars = ObjectSerializer.serializeObject(p_loadedJars);
+        m_loadedJars = p_loadedJars;
+        m_setSize = m_loadedJars.size();
     }
 
-    public HashSet<String> getLoadedJars() {
-        return (HashSet<String>) ObjectSerializer.deserializeObject(m_loadedJars, HashSet.class);
+    public Set<String> getLoadedJars() {
+        return m_loadedJars;
     }
 
     @Override
     protected final int getPayloadLength() {
-        return ObjectSizeUtil.sizeofByteArray(m_loadedJars);
+        int size = 0;
+
+        size += Integer.BYTES;
+        for (String s : m_loadedJars) {
+            size += ObjectSizeUtil.sizeofString(s);
+        }
+
+        return size;
     }
 
     @Override
     protected void writePayload(AbstractMessageExporter p_exporter) {
-        p_exporter.writeByteArray(m_loadedJars);
+        p_exporter.writeInt(m_setSize);
+        for (String s : m_loadedJars) {
+            p_exporter.writeString(s);
+        }
     }
 
     @Override
     protected void readPayload(AbstractMessageImporter p_importer) {
-        m_loadedJars = p_importer.readByteArray(m_loadedJars);
+        m_setSize = p_importer.readInt(m_setSize);
+        Set<String> jars = new HashSet<>();
+
+        for (int i = 0; i < m_setSize; i++) {
+            String s = "";
+            jars.add(p_importer.readString(s));
+        }
+
+        m_loadedJars = jars;
     }
 }
--- src/main/java/de/hhu/bsinfo/dxram/loader/messages/SyncResponseMessage.java
+++ src/main/java/de/hhu/bsinfo/dxram/loader/messages/SyncResponseMessage.java
@@ -1,17 +1,17 @@
 package de.hhu.bsinfo.dxram.loader.messages;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import de.hhu.bsinfo.dxnet.core.AbstractMessageExporter;
 import de.hhu.bsinfo.dxnet.core.AbstractMessageImporter;
 import de.hhu.bsinfo.dxnet.core.Message;
-import de.hhu.bsinfo.dxnet.core.Response;
 import de.hhu.bsinfo.dxram.DXRAMMessageTypes;
-import de.hhu.bsinfo.dxram.loader.ObjectSerializer;
 import de.hhu.bsinfo.dxutils.serialization.ObjectSizeUtil;
 
 public class SyncResponseMessage extends Message {
-    private byte[] m_jarByteArrays;
+    private HashMap<String, byte[]> m_jarByteArrays;
+    private int m_mapSize;
 
     public SyncResponseMessage() {
         super();
@@ -19,25 +19,47 @@ public class SyncResponseMessage extends Message {
 
     public SyncResponseMessage(final short p_destination, final HashMap<String, byte[]> p_jarByteArrays) {
         super(p_destination, DXRAMMessageTypes.LOADER_MESSAGE_TYPE, LoaderMessages.SUBTYPE_SYNC_RESPONSE);
-        m_jarByteArrays = ObjectSerializer.serializeObject(p_jarByteArrays);
+        m_jarByteArrays = p_jarByteArrays;
+        m_mapSize = m_jarByteArrays.size();
     }
 
     public HashMap<String, byte[]> getJarByteArrays() {
-        return (HashMap<String, byte[]>) ObjectSerializer.deserializeObject(m_jarByteArrays, HashMap.class);
+        return m_jarByteArrays;
     }
 
     @Override
     protected final int getPayloadLength() {
-        return ObjectSizeUtil.sizeofByteArray(m_jarByteArrays);
+        int size = 0;
+
+        size += Integer.BYTES;
+        for (Map.Entry<String, byte[]> e : m_jarByteArrays.entrySet()) {
+            size += ObjectSizeUtil.sizeofString(e.getKey());
+            size += ObjectSizeUtil.sizeofByteArray(e.getValue());
+        }
+
+        return size;
     }
 
     @Override
     protected void writePayload(AbstractMessageExporter p_exporter) {
-        p_exporter.writeByteArray(m_jarByteArrays);
+        p_exporter.writeInt(m_mapSize);
+        for (Map.Entry<String, byte[]> e : m_jarByteArrays.entrySet()) {
+            p_exporter.writeString(e.getKey());
+            p_exporter.writeByteArray(e.getValue());
+        }
     }
 
     @Override
     protected void readPayload(AbstractMessageImporter p_importer) {
-        m_jarByteArrays = p_importer.readByteArray(m_jarByteArrays);
+        m_mapSize = p_importer.readInt(m_mapSize);
+        HashMap<String, byte[]> jarMap = new HashMap<>();
+
+        for (int i = 0; i < m_mapSize; i++) {
+            String s = "";
+            byte[] b = new byte[0];
+            jarMap.put(p_importer.readString(s), p_importer.readByteArray(b));
+        }
+
+        m_jarByteArrays = jarMap;
     }
 }
